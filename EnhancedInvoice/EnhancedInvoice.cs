af15e0c8ec16b70f5856a9bdb8c79ae067b9ff8c
mariusraht2 <mariusraht+2@gmail.com>
2020-01-25 17:36:10 +0100

    Projektdateien hinzufügen.

---
+++ EnhancedInvoice/EnhancedInvoice.cs
@@ -0,0 +1,114 @@
+﻿using Financials;
+using System;
+using System.Collections.Generic;
+
+namespace EnhancedInvoice
+{
+    class EnhancedInvoice : Invoice
+    {
+        private new List<EnhancedInvoiceItem> items = new List<EnhancedInvoiceItem>();
+        public new List<EnhancedInvoiceItem> Items
+        {
+            get
+            {
+                items.Clear();
+
+                for (int i = 0; i < base.Items.Count; i++)
+                {
+                    items.Add((EnhancedInvoiceItem)base.Items[i]);
+                }
+
+                return items;
+            }
+            set
+            {
+                items = value;
+            }
+        }
+        public EnhancedInvoiceItem this[int index]
+        {
+            get
+            {
+                return Items[index];
+            }
+        }
+        private double totalSum = 0;
+        public double TotalSum
+        {
+            get
+            {
+                totalSum = 0;
+
+                foreach (EnhancedInvoiceItem item in Items)
+                {
+                    totalSum += item.SumPrice;
+                }
+
+                return totalSum;
+            }
+            set
+            {
+                totalSum = value;
+            }
+        }
+
+        public EnhancedInvoice(DateTime date, uint number, string currency, string recipient) : base()
+        {
+            Date = date;
+            Number = number;
+            Currency = currency;
+            Recipient = recipient;
+        }
+
+        public override string ToString()
+        {
+            return $"= {TotalSum}";
+        }
+
+        //public IEnumerator<EnhancedInvoiceItem> GetEnumerator()
+        //{
+        //    foreach (EnhancedInvoiceItem item in Items)
+        //    {
+        //        return item;
+        //    }
+        //}
+
+        //public IEnumerable<EnhancedInvoiceItem> GetEnumerable()
+        //{
+        //    foreach(EnhancedInvoiceItem item in Items)
+        //    {
+        //        return item;
+        //    } 
+        //}
+    }
+
+    class EnhancedInvoiceItem : InvoiceItem
+    {
+        private double sumPrice = 0;
+        public new double SumPrice
+        {
+            get
+            {
+                sumPrice = Quantity * UnitPrice;
+                return sumPrice;
+            }
+            set
+            {
+                sumPrice = value;
+            }
+        }
+
+        public EnhancedInvoiceItem(uint stocknumber, uint quantity, string description, double unitprice) : base()
+        {
+            Stocknumber = stocknumber;
+            Quantity = quantity;
+            Description = description;
+            UnitPrice = unitprice;
+        }
+
+        public override string ToString()
+        {
+            return $"{Quantity} of Item {Description} ({Stocknumber}) for {UnitPrice} each.";
+        }
+    }
+}
---
+++ EnhancedInvoice/Program.cs
@@ -0,0 +1,64 @@
+﻿using Financials;
+using System;
+
+namespace EnhancedInvoice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice myInvoice;
+            InvoiceItem myItem;
+
+            myInvoice = new Invoice();
+            myInvoice.Date = System.DateTime.Today;
+            myInvoice.Number = 1234;
+            myInvoice.Currency = "EUR";
+            myInvoice.Recipient = "Herr Meier";
+
+            myItem = new InvoiceItem();
+            myItem.Stocknumber = 5678;
+            myItem.Description = "Kaffetasse, groß";
+            myItem.Quantity = 23;
+            myItem.UnitPrice = 12.99;
+            myInvoice.AddItem(myItem);
+
+            myItem = new InvoiceItem();
+            myItem.Stocknumber = 7856;
+            myItem.Description = "Kaffetasse, klein";
+            myItem.Quantity = 12;
+            myItem.UnitPrice = 9.99;
+            myInvoice.AddItem(myItem);
+
+            Console.WriteLine(((InvoiceItem)myInvoice.Items[0]).SumPrice);
+            Console.ReadLine();
+        }
+    }
+
+    class ProgramEnhanced
+    {
+        static void Main(string[] args)
+        {
+            EnhancedInvoice myInvoice;
+
+            myInvoice = new EnhancedInvoice(DateTime.Today, 1234, "EUR", "Herr Meier");
+            myInvoice.AddItem(new EnhancedInvoiceItem(5678, 23, "Kaffetasse, groß", 12.99));
+            myInvoice.AddItem(new EnhancedInvoiceItem(7856, 12, "Kaffetasse, klein", 9.99));
+
+            Invoice myBase = myInvoice;
+
+            // Aufruf durch das überschriebene Property
+            Console.WriteLine(myInvoice.Items[1].SumPrice);
+
+            // Aufruf durch den Indexer
+            Console.WriteLine(myInvoice[1].SumPrice);
+
+            // Ausgabe über ToString
+            Console.WriteLine(myInvoice.ToString());
+            Console.WriteLine(myBase.ToString());
+
+            
+            Console.ReadKey();
+        }
+    }
+}
